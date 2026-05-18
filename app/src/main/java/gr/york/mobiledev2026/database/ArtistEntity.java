21e932fe2c85b43571ce6c83e573d1d7b0fd4b86
george <gchatzopoulos@york.citycollege.eu>
2026-05-18 17:04:53 +0300

    feat(ArtistEntity): add an entity for the Artists
    - Fields: name, imagePath
    
    Add name as a primary key as each artist's name is unique (trim to resolve outer whitespace issues)
    For performance reasons and best practices, save only the path of the artist's image in the database, and the file in the file system

---
+++ app/src/main/java/gr/york/mobiledev2026/database/ArtistEntity.java
@@ -0,0 +1,45 @@
+package gr.york.mobiledev2026.database;
+
+import androidx.annotation.NonNull;
+import androidx.room3.ColumnInfo;
+import androidx.room3.Entity;
+
+@Entity(primaryKeys = {"name"})
+public class ArtistEntity {
+    @NonNull
+    @ColumnInfo(name = "name", collate = ColumnInfo.NOCASE)
+    private String name;
+
+    @ColumnInfo(name = "image_path")
+    private String imagePath;
+
+    public ArtistEntity(@NonNull String name) {
+        this.name = name.trim();
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "ArtistEntity{" +
+                "name='" + name + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                '}';
+    }
+}
