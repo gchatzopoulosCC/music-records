eb402bb2b05939279dc5c320e3d3309a412325d8
george <gchatzopoulos@york.citycollege.eu>
2026-05-18 17:15:07 +0300

    chore(Db, DatabaseActivity): remove sample code

--- app/src/main/java/gr/york/mobiledev2026/database/DatabaseActivity.java
+++ app/src/main/java/gr/york/mobiledev2026/database/DatabaseActivity.java
@@ -2,12 +2,12 @@ package gr.york.mobiledev2026.database;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
+import androidx.room3.Room;
 
+import java.util.Objects;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -16,9 +16,9 @@ import gr.york.mobiledev2026.databinding.ActivityDatabaseBinding;
 public class DatabaseActivity extends AppCompatActivity {
 
     private ActivityDatabaseBinding binding;
-    private MyDatabase database;
+    private Db database;
 
-    private Executor executor = Executors.newSingleThreadExecutor();
+    private final Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,47 +27,40 @@ public class DatabaseActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        binding.databaseTxtTitle.setText(getIntent().getExtras().getString("name"));
+        binding.databaseTxtTitle.setText(Objects.requireNonNull(getIntent().getExtras()).getString("name"));
         binding.databaseTxtNumber.setText(String.valueOf(getIntent().getExtras().getInt("number")));
 
-        binding.databaseBtnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.databaseBtnClose.setOnClickListener(view -> {
 
-                Intent intent = new Intent();
-                intent.putExtra("result", "Data from DatabaseActivity");
+            Intent intent = new Intent();
+            intent.putExtra("result", "Data from DatabaseActivity");
 
-                setResult(3000, intent);
-                finish();
-            }
+            setResult(3000, intent);
+            finish();
         });
 
         database = Room.databaseBuilder(
                 getApplicationContext(),
-                MyDatabase.class,
-                "my_database"
+                Db.class,
+                "db"
         ).build();
 
-        executor.execute(new Runnable() {
-
-            @Override
-            public void run() {
-                UserEntity user = new UserEntity();
-                user.setName("John");
+//        executor.execute(() -> {
+//                UserEntity user = new UserEntity();
+//                user.setName("John");
 //                try {
 //                    database.userDao().insert(user);
 //                } catch (Exception e) {
 //                    throw new RuntimeException(e);
 //                }
 
-                database.userDao().save(user);
+//                database.userDao().save(user);
 
-                UserEntity[] dataList = database.userDao().readAll();
+//                UserEntity[] dataList = database.userDao().readAll();
 
-                Log.d("DatabaseActivity", "Data from database: " + dataList.length);
-
-            }
-        });
+//                Log.d("DatabaseActivity", "Data from database: " + dataList.length);
+//
+//        });
 
     }
 
--- app/src/main/java/gr/york/mobiledev2026/database/Db.java
+++ app/src/main/java/gr/york/mobiledev2026/database/Db.java
@@ -1,9 +1,36 @@
 package gr.york.mobiledev2026.database;
 
+import static androidx.room3.Room.databaseBuilder;
+
+import android.content.Context;
+
 import androidx.room3.Database;
 import androidx.room3.RoomDatabase;
 
-@Database(entities = {ArtistEntity.class, ArtistFts.class}, version = 1)
-public abstract class MyDatabase extends RoomDatabase {
+@Database(
+        entities = {
+                ArtistEntity.class,
+                ArtistFts.class
+        },
+        version = 1
+)
+public abstract class Db extends RoomDatabase {
+    public static volatile Db INSTANCE;
+
+    public static Db getDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (Db.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = databaseBuilder(
+                            context.getApplicationContext(),
+                            Db.class, "db"
+                    ).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
     public abstract ArtistDao artistDao();
+
 }
