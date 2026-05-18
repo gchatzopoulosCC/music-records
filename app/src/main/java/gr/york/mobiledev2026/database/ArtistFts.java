ed0ccd2974c829927819d616f90f33b9a38752b8
george <gchatzopoulos@york.citycollege.eu>
2026-05-18 17:06:34 +0300

    feat(ArtistFts): add full-text search for artists

---
+++ app/src/main/java/gr/york/mobiledev2026/database/ArtistFts.java
@@ -0,0 +1,16 @@
+package gr.york.mobiledev2026.database;
+
+import androidx.annotation.NonNull;
+import androidx.room3.Entity;
+import androidx.room3.Fts5;
+
+@Entity
+@Fts5(contentEntity = ArtistEntity.class)
+public class ArtistFts {
+    @NonNull
+    public String name;
+
+    public ArtistFts(@NonNull String name) {
+        this.name = name;
+    }
+}
