473aadcb6ae68ed99bdec3103d7267282c42f183
george <gchatzopoulos@york.citycollege.eu>
2026-05-18 17:09:38 +0300

    feat(ArtistDao): add a dao for the ArtistEntity and ArtistFts
    Functions:
    - Standard GRUD functions
    - Retrieve with a limit
    - Fts usage for searchByName(String query)

---
+++ app/src/main/java/gr/york/mobiledev2026/database/ArtistDao.java
@@ -0,0 +1,44 @@
+package gr.york.mobiledev2026.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room3.Dao;
+import androidx.room3.Delete;
+import androidx.room3.Insert;
+import androidx.room3.OnConflictStrategy;
+import androidx.room3.Query;
+import androidx.room3.Update;
+import androidx.room3.Upsert;
+
+import java.util.List;
+
+@Dao
+public interface ArtistDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(ArtistEntity artist);
+
+    @Update
+    void update(ArtistEntity artist);
+
+    @Upsert
+    void save(ArtistEntity artist);
+
+    @Delete
+    void delete(ArtistEntity artist);
+
+    @Query("SELECT * FROM ArtistEntity")
+    LiveData<List<ArtistEntity>> readAll();
+
+    @Query("SELECT * FROM ArtistEntity LIMIT :lim")
+    LiveData<List<ArtistEntity>> readUpTo(int lim);
+
+    @Query("SELECT * FROM ArtistEntity WHERE name = :query")
+    LiveData<ArtistEntity> findByName(String query);
+
+    @Query("SELECT ArtistEntity.* FROM ArtistEntity " +
+            "JOIN ArtistFts ON ArtistEntity.rowid = ArtistFts.rowid " +
+            "WHERE ArtistFts MATCH :query")
+    LiveData<List<ArtistEntity>> searchByName(String query);
+
+    @Query("SELECT image_path FROM ArtistEntity WHERE name = :name")
+    String getImagePathByName(String name);
+}
