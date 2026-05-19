48c3f007d53cc5325db0df8a056e5704374bad46
george <gchatzopoulos@york.citycollege.eu>
2026-05-19 21:54:40 +0300

    feat(Album): add additional variables
    - coverImageUrl
    - listeners
    - playcount
    - genre
    - url
    - tracks

--- app/src/main/java/gr/york/mobiledev2026/data/model/Album.java
+++ app/src/main/java/gr/york/mobiledev2026/data/model/Album.java
@@ -1,32 +1,93 @@
 package gr.york.mobiledev2026.data.model;
 
 import java.util.Date;
+import java.util.List;
 
 public class Album {
-    private String artist;
-    private String album;
+    private String name;
+    private Artist artist;
     private Date releaseDate;
+    private String coverImageUrl;
+    private int listeners;
+    private int playcount;
+    private String genre;
+    private String url;
+    private List<Track> tracks;
 
-    public Album() {}
-
-    public Album(String artist, String album) {
+    public Album(Artist artist, String name) {
         this.artist = artist;
-        this.album = album;
+        this.name = name;
     }
 
-    public String getArtist() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Artist getArtist() {
         return artist;
     }
 
-    public void setArtist(String artist) {
+    public void setArtist(Artist artist) {
         this.artist = artist;
     }
 
-    public String getAlbum() {
-        return album;
+    public Date getReleaseDate() {
+        return releaseDate;
     }
 
-    public void setAlbum() {
-        this.album = album;
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    public int getListeners() {
+        return listeners;
+    }
+
+    public void setListeners(int listeners) {
+        this.listeners = listeners;
+    }
+
+    public int getPlaycount() {
+        return playcount;
+    }
+
+    public void setPlaycount(int playcount) {
+        this.playcount = playcount;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
     }
 }
