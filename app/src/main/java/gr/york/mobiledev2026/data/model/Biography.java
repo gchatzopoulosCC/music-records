27eb829af8bcbf386f92a092834f31b15e79b5be
george <gchatzopoulos@york.citycollege.eu>
2026-05-19 22:18:58 +0300

    feat(Biography): add a Biography class

---
+++ app/src/main/java/gr/york/mobiledev2026/data/model/Biography.java
@@ -0,0 +1,39 @@
+package gr.york.mobiledev2026.data.model;
+
+import java.util.Date;
+
+public class Biography {
+    private Date published;
+    private String summary;
+    private String content;
+
+    public Biography(Date published, String summary, String content) {
+        this.published = published;
+        this.summary = summary;
+        this.content = content;
+    }
+
+    public Date getPublished() {
+        return published;
+    }
+
+    public void setPublished(Date published) {
+        this.published = published;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
