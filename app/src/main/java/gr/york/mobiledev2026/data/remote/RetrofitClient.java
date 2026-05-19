bc91aac1154eb8b644d87f97b9061c9110390099
george <gchatzopoulos@york.citycollege.eu>
2026-05-19 23:08:44 +0300

    feat(RetrofitClient): add a client for Retrofit

--- app/src/main/java/gr/york/mobiledev2026/data/remote/RetrofitClient.java
+++ app/src/main/java/gr/york/mobiledev2026/data/remote/RetrofitClient.java
@@ -1,4 +1,19 @@
 package gr.york.mobiledev2026.data.remote;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+    private static final String BASE_URL = "http://ws.audioscrobbler.com/2.0/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        }
+        return retrofit;
+    }
 }
