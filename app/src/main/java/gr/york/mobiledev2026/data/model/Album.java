a0833d050486631a70ce690a8ba18ff99ae5cfe4
gchatzopoulosCC <gchatzopoulos@york.citycollege.eu>
2026-05-19 19:54:57 +0300

    feat(Album): add Album Model

--- app/src/main/java/gr/york/mobiledev2026/data/model/Album.java
+++ app/src/main/java/gr/york/mobiledev2026/data/model/Album.java
@@ -1,4 +1,29 @@
 package gr.york.mobiledev2026.data.model;
 
 public class Album {
+    private String artist;
+    private String album;
+
+    public Album() {}
+
+    public Album(String artist, String album) {
+        this.artist = artist;
+        this.album = album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum() {
+        this.album = album;
+    }
 }
