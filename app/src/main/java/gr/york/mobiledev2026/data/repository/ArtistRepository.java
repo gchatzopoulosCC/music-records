8372ac9ee76ddde320b49a628a8d747863d2bebd
george <gchatzopoulos@york.citycollege.eu>
2026-05-20 17:36:15 +0300

    feat(AlbumRepository, ArtistRepository, ChartRepository, TrackRepository): implement the api service interface functions

--- app/src/main/java/gr/york/mobiledev2026/data/repository/ArtistRepository.java
+++ app/src/main/java/gr/york/mobiledev2026/data/repository/ArtistRepository.java
@@ -1,4 +1,41 @@
 package gr.york.mobiledev2026.data.repository;
 
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import gr.york.mobiledev2026.data.api.ArtistService;
+import gr.york.mobiledev2026.data.model.Artist;
+import gr.york.mobiledev2026.data.model.Resource;
+import gr.york.mobiledev2026.data.model.Track;
+import gr.york.mobiledev2026.data.remote.NetworkUtils;
+import gr.york.mobiledev2026.data.remote.RetrofitClient;
+
 public class ArtistRepository {
+
+    private static ArtistRepository instance;
+    private final ArtistService service;
+
+    private ArtistRepository() {
+        service = RetrofitClient.getClient().create(ArtistService.class);
+    }
+
+    public static synchronized ArtistRepository getInstance() {
+        if (instance == null) {
+            instance = new ArtistRepository();
+        }
+        return instance;
+    }
+
+    public LiveData<Resource<List<Artist>>> getArtists() {
+        return NetworkUtils.processObservable(service.getArtists());
+    }
+
+    public LiveData<Resource<Artist>> getArtistByName(String name) {
+        return NetworkUtils.processObservable(service.getArtistByName(name), Artist::isValid);
+    }
+
+    public LiveData<Resource<List<Track>>> getTopTracksByArtist(String name) {
+        return NetworkUtils.processObservable(service.getTopTracksByArtist(name));
+    }
 }
--- app/src/main/java/gr/york/mobiledev2026/data/repository/ChartRepository.java
+++ app/src/main/java/gr/york/mobiledev2026/data/repository/ChartRepository.java
@@ -1,4 +1,30 @@
 package gr.york.mobiledev2026.data.repository;
 
+import androidx.lifecycle.LiveData;
+
+import gr.york.mobiledev2026.data.api.ChartService;
+import gr.york.mobiledev2026.data.model.Chart;
+import gr.york.mobiledev2026.data.model.Resource;
+import gr.york.mobiledev2026.data.remote.NetworkUtils;
+import gr.york.mobiledev2026.data.remote.RetrofitClient;
+
 public class ChartRepository {
+
+    private static ChartRepository instance;
+    private final ChartService service;
+
+    private ChartRepository() {
+        service = RetrofitClient.getClient().create(ChartService.class);
+    }
+
+    public static synchronized ChartRepository getInstance() {
+        if (instance == null) {
+            instance = new ChartRepository();
+        }
+        return instance;
+    }
+
+    public LiveData<Resource<Chart>> getChart() {
+        return NetworkUtils.processObservable(service.getChart(), Chart::isValid);
+    }
 }
