b71f928bd377cdb6395667c856a32608020734bd
george <gchatzopoulos@york.citycollege.eu>
2026-05-20 16:50:17 +0300

    chore(ArtistService): include top tracks in the Artist Service

--- app/src/main/java/gr/york/mobiledev2026/data/api/ArtistService.java
+++ app/src/main/java/gr/york/mobiledev2026/data/api/ArtistService.java
@@ -3,6 +3,7 @@ package gr.york.mobiledev2026.data.api;
 import java.util.List;
 
 import gr.york.mobiledev2026.data.model.Artist;
+import gr.york.mobiledev2026.data.model.Track;
 import gr.york.mobiledev2026.data.remote.ApiResponse;
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.GET;
@@ -14,4 +15,7 @@ public interface ArtistService {
 
     @GET("artist/{name}")
     Observable<ApiResponse<Artist>> getArtistByName(@Path("name") String name);
+
+    @GET("artist/{name}/toptracks")
+    Observable<ApiResponse<List<Track>>> getTopTracksByArtist(@Path("name") String name);
 }
